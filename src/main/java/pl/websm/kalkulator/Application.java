6711a43ed905e74afaa09d7a6c8dfd1a7e441cf9
sebmichalowski <seb.michalowski@gmail.com>
2018-05-06 00:29:58 +0200

    Rename KalculatorApplication to Application and enable sheduling

--- src/main/java/pl/websm/kalkulator/Application.java
+++ src/main/java/pl/websm/kalkulator/Application.java
@@ -2,12 +2,14 @@ package pl.websm.kalkulator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class KalkulatorApplication {
+@EnableScheduling
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(KalkulatorApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
