af4d833545c20cbf46f324fb27a80e742c1c573a
sebmichalowski <seb.michalowski@gmail.com>
2018-05-06 00:28:14 +0200

    Initial sql data file with countries implemented

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+Country (country_code, language_code, currency_code, tax_value, fixed_costs)
+VALUES
+('UK', 'uk', 'GBP', 0.25, 600);
+
+INSERT INTO
+Country (country_code, language_code, currency_code, tax_value, fixed_costs)
+VALUES
+('DE', 'de', 'EUR', 0.20, 800);
+
+INSERT INTO
+Country (country_code, language_code, currency_code, tax_value, fixed_costs)
+VALUES
+('PL', 'pl', 'PLN', 0.19, 1200);
