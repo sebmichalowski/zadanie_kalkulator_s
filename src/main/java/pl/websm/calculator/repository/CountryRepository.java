e68d0090a2e532bf614f5deb6cc22905cf52dcf9
sebmichalowski <seb.michalowski@gmail.com>
2018-05-07 03:54:14 +0200

    Add findByCountryCode method, change package name

--- src/main/java/pl/websm/calculator/repository/CountryRepository.java
+++ src/main/java/pl/websm/calculator/repository/CountryRepository.java
@@ -1,12 +1,13 @@
-package pl.websm.kalkulator.repository;
+package pl.websm.calculator.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import pl.websm.kalkulator.model.Country;
+import pl.websm.calculator.model.Country;
 
 import java.util.Optional;
 
 @Repository
 public interface CountryRepository extends CrudRepository<Country, Long> {
     Optional<Country> findById(Long id);
+    Optional<Country> findByCountryCode(String baseCountryCode);
 }
